9c7de4a7f68262d150f152789a1bbe8c1db0682a
Admin3 <nathanrath@live.co.uk>
2020-11-06 14:04:20 +0000

    Added changelogs to releases

--- compile.bat
+++ compile.bat
@@ -7,4 +7,6 @@ pause
 pyinstaller dw.spec -y --clean
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "./dw-piper.iss" /DAppVersion=%version%
 echo Compiled version %version%
+( echo ^> Changelog ^(%date%^) & echo ^> Version: %version% & echo. & echo - First change & echo - Another change ) > ./output/%version%/changelog.txt
+echo Generated template changelog
 pause
